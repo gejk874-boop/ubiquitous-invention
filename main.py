ee9c5308efb3b8d1d3926e45d29414f2bc2a0cf8
gejk874-boop <gejk874@gmail.com>
2025-11-23 19:41:10 +0700

    main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from aiogram.types import (
 )
 
 # === КОНФИГУРАЦИЯ ===
-BOT_TOKEN = "8243992851:AAGoLj2MowK2_ft_dKfnIo1TjfK6tArfMsQ"
+BOT_TOKEN = "8368551303:AAFWZi4TZJhfEqYEOLyQOutGW0HxOyhg99M"
 ADMIN_IDS = [6986121067]
 
 bot = Bot(token=BOT_TOKEN)
